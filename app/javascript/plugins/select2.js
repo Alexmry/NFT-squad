e98ace230f4fda15770e0f70cd5b32e90991f058
Alexandre Morey <alexander006_9@hotmail.com>
2021-06-15 21:45:20 +0100

    dont merge it

--- app/javascript/plugins/select2.js
+++ app/javascript/plugins/select2.js
@@ -1,14 +1,18 @@
-import 'select2';
-import $ from 'jquery';
+// import 'select2';
+// import $ from 'jquery';
 
-const initSelect2 = () => {
-    $('select.select2-category').select2({
-        placeholder: 'Select a category',
-        width: '40%',
-        height: '20px'
-    });
-    // const value = $('select.select2-category').find(':selected')[0].innerText;
-    // console.log(value);
-}
+// const initSelect2 = () => {
+//   const dropdown = document.querySelector(".select2-category");
+//   if (dropdown) {
+//     $('select.select2-category').select2({
+//       placeholder: 'Select a category',
+//       width: '40%',
+//       height: '20px'
+//     });
+//     // const value = $('select.select2-category').find(':selected')[0].innerText;
+//     // console.log(value);
+//   }
 
-export { initSelect2 };
+// }
+
+// export { initSelect2 };
